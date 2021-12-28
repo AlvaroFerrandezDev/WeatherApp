7d5f49cdffcf21b467859bb0bf784aa2d3084517
Álvaro Ferrández <alvaro.ferrandez.dev@gmail.com>
2021-12-28 16:46:13 +0100

    Initial Commit

---
+++ WeatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by Álvaro Ferrández Gómez on 28/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
